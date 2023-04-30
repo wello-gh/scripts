37633726c29e43cc4d5c4c872f2493eb9a147f8f
wello-gh <wellothedeveloper@gmail.com>
2023-04-30 20:37:34 +0500

    initial

---
+++ main.lua
@@ -0,0 +1,35 @@
+local ServerScriptService = game:GetService("ServerScriptService")
+-- Atlas Productions
+
+--[[
+    This script was made by the Atlas Developers.
+    Please do not redistribute or copy without our permission.
+    Visit https://discord.gg/RKEVNpBbHz for more information.
+    
+    Thank you for using Atlas!
+    Licensed by Xeleron.
+]]
+
+--[[
+    This script is meant for the Roblox game named Doors.
+    It will break on other games.
+
+    Script name: 
+    Original developer:
+    License:
+    Licensed by:
+    Description:
+]]
+
+-- Variables
+
+local undefined = nil
+
+local errors = {
+    ["UniversalNotFound"] = "UniversalNotFound",
+    ["UniversalNotValid"] = "UniversalNotValid",
+    ["ScriptNotFound"] = "ScriptNotFound",
+    ["ScriptNotValid"] = "ScriptNotValid",
+    ["CriticalError"] = "CriticalError",
+    ["HttpError"] = "HttpError",
+}
